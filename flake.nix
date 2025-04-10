f6299b9461c97a17ae185f6a7f87c7314183f45b
BirdeeHub <robindbor@gmail.com>
2025-04-10 00:11:02 -0700

    first commit

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = { self, nixpkgs, ... }: let
+    forAllSys = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
+  in {
+    packages = forAllSys (system: let
+      pkgs = import nixpkgs { inherit system; };
+      makeBinWrapper = pkgs.callPackage ./. {};
+    in {
+      default = makeBinWrapper;
+    });
+  };
+}
